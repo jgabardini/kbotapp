15e7fe6204e8dfa7717b699911f0fc9be036e52e
Juan Gabardini <jgabardini@computer.org>
2020-11-23 13:26:20 +0000

    simplificar / y agregar sessions

--- app.rb
+++ app.rb
@@ -2,29 +2,39 @@ require 'sinatra'
 require './config'
 require 'time'
 require './lib/kbot'
+enable :sessions
 
 kbot= nil
-momento= 0
-get '/:cuando?' do |cuando|
+@momento= 0
+get '/' do 
     kbot= Kbot.new 
-    if cuando
-        momento= cuando.to_i
-    else
-        momento= case Time.now.hour
-                when 5..13 then 0
-                when 14..20 then 1
-                else 2
-            end
-    end
+    session[:momento]= momento_del_dia
+    puts "/ momento #{session[:momento]}"
     erb :index
 end
 
 post '/chat' do
-
+    puts "/chat momento #{session[:momento]}"
     if params[:ingreso]
-        kbot.ingresa params[:ingreso], momento
+        kbot.ingresa params[:ingreso], session[:momento]
     end
     @mensajes=  kbot.mensajes
-
+    
     erb :chat
+end
+
+def momento_del_dia
+    puts "momento_del_dia momento #{session[:momento]}"
+    case Time.now.hour
+        when 5..13 then 0
+        when 14..20 then 1
+        else 2
+    end
+end
+
+get '/fijarhora/:cuando' do |cuando|
+    kbot= Kbot.new 
+    session[:momento]= cuando.to_i
+    puts "/fijarhora momento #{session[:momento]}"
+    erb :index
 end
